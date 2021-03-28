980d4999b77784a6713d5fe49611e9ee5aef6fd4
GabrielKishida <gabriel.kishida@usp.br>
2021-03-28 13:09:04 -0300

    :rocket: Minimum password validation

--- passwordvalidation.py
+++ passwordvalidation.py
@@ -12,10 +12,18 @@ def valid_string_size(password) :
     return len(password) <=14 and len(password) >= 8
 
 def has_upper(password) :
-    return not password.islower()
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for char in upper :
+        if char in password :
+            return True
+    return False
     
 def has_lower(password) :
-    return not password.isupper()
+    upper = "abcdefghijklmnopqrstuvwxyz"
+    for char in upper :
+        if char in password :
+            return True
+    return False
 
 def has_number(password) :
     has_number = False
@@ -26,12 +34,12 @@ def has_number(password) :
     return has_number
 
 def has_specialchar(password) :
-    regex = re.compile("/\[@_!#$%^&*()<>?}{~:]|")
-    if (regex.search(password) == None) :
-        return False
-    else :
-        return True
-    
+    specialchar = "/\[@_!#$%^&*()<>?}{~:]|"
+    for char in specialchar :
+        if char in password :
+            return True
+    return False
+
 def repeats_sequence(password) :
     n = len(password)
     for i in range(n-2) :
@@ -39,3 +47,30 @@ def repeats_sequence(password) :
         if (password.count(segpassword) > 1) :
             return True
     return False
+
+def password_validation(password) :
+    valid = True
+    if(not valid_string_size(password)) :
+        print("A sua senha tem tamanho inválido (deve conter de 8 a 14 caracteres).")
+        valid = False
+    if(not has_upper(password)) :
+        print("A sua senha deve conter pelo menos um caractere maiúsculo.")
+        valid = False
+    if(not has_lower(password)) :
+        print("A sua senha deve conter pelo menos um caractere minúsculo.")
+        valid = False
+    if(not has_number(password)) :
+        print("A sua senha deve conter pelo menos um número.")
+        valid = False
+    if(not has_specialchar(password)) :
+        print("A sua senha deve conter pelo menos um caractere especial.")
+        valid = False
+    if(repeats_sequence(password)) :
+        print("A sua senha não pode conter uma sequência de caracteres repetidos.")
+        valid = False
+    if(valid) :
+        print("Senha válida.")
+    return valid
+
+password = input("Por favor insira a sua senha: ")
+password_validation(password)
