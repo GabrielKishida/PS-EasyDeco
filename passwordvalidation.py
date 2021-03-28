2d937a4dc25acc0bf34fdd77a6916aff12116063
GabrielKishida <gabriel.kishida@usp.br>
2021-03-28 14:54:54 -0300

    :bulb: Add comment

--- passwordvalidation.py
+++ passwordvalidation.py
@@ -1,19 +1,21 @@
 ####################################################
 # Processo Seletivo EasyDeco
-# Algoritmo( para a validação de senhas
+# Algoritmo para a validação de senhas
 # Desenvolvido por: Gabriel Yugo Nascimento Kishida
 # Contato : gabriel.kishida@usp.br
 # Março de 2021
 ####################################################
 
-import re
+### Definição de Funções Auxiliares ###
 
+# Função que valida se a senha tem tamanho válido
 def valid_string_size(password) :
     valid = len(password) <=14 and len(password) >= 8
     if not valid :
         print("A sua senha tem tamanho inválido (deve conter de 8 a 14 caracteres).")
     return valid
 
+# Função que valida se a senha tem pelo menos uma letra maiúscula
 def has_upper(password) :
     upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for char in upper :
@@ -21,7 +23,8 @@ def has_upper(password) :
             return True
     print("A sua senha deve conter pelo menos um caractere maiúsculo.")
     return False
-    
+
+# Função que valida se a senha tem pelo menos uma letra minúscula
 def has_lower(password) :
     upper = "abcdefghijklmnopqrstuvwxyz"
     for char in upper :
@@ -30,6 +33,7 @@ def has_lower(password) :
     print("A sua senha deve conter pelo menos um caractere minúsculo.")
     return False
 
+# Função que valida se a senha tem pelo menos um algarismo
 def has_number(password) :
     for char in password :
         if char.isdigit(): 
@@ -37,6 +41,7 @@ def has_number(password) :
     print("A sua senha deve conter pelo menos um número.")
     return False
 
+# Função que valida se a senha tem pelo menos um caractere especial
 def has_specialchar(password) :
     specialchar = "/\[@_!#$%^&*()<>?}{~:]|"
     for char in specialchar :
@@ -45,6 +50,7 @@ def has_specialchar(password) :
     print("A sua senha deve conter pelo menos um caractere especial.")
     return False
 
+# Função que valida se a senha repete uma sequência de caracteres (não pode repitir)
 def repeats_sequence(password) :
     n = len(password)
     for i in range(n-2) :
@@ -54,13 +60,19 @@ def repeats_sequence(password) :
             return True
     return False
 
+# Função que verifica se todas as validações foram feitas de maneira correta.
 def multiple_validation(validations) :
     valid = True
     for validation in validations :
         valid = valid and validation
+        if (not valid) : return valid
     return valid
 
-
+### Função principal ###
+# Esta função é responsável por receber uma senha (password) em String, e
+# avalia se a senha passa por todos os critérios requeridos. Caso passe,
+# imprime que a senha é válida e retorna True. Caso contrário, imprime
+# qual critério não foi validado e retorna False.
 def password_validation(password) :
     validations = []
     validations.append(valid_string_size(password))
@@ -74,5 +86,7 @@ def password_validation(password) :
         print("Senha válida.")
     return valid
 
+### Main ###
+# Esta seção constitui um exemplo no qual a função principal poderia ser chamada.
 password = input("Por favor insira a sua senha: ")
 password_validation(password)
